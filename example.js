f684a3d4d8508fc084ad11792c5cf00070f2f119
Roman <bolgovr@gmail.com>
2011-12-26 01:20:26 +0200

    added examples of experimental module "flower"

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 var Loader = require('./lib/loader.js');
+var async = require('async');
+var FlowerLib = require('./lib/flower.js');
 
-
+var flower = new FlowerLib();
 var flow = new Loader();
 
 flow.setRoot(__dirname);
@@ -10,9 +12,20 @@ flow.registerPath('global', './test/sampleDirStructure', function () {
         'login':'r00t',
         'password':'12345'
     };
+    flower.setModuleContainer(flow.getFlowTree('global'));
+    var startfunc = function (callback, data) {
+        callback(null, {'user':user})
+    };
+    flower.createFlow('test', startfunc).then('userAuthorization').and('insertBlogPost')
+        .then('renderingAnswer').run(function (err, data) {
+            console.log(arguments);
+        });
+
+
     flow.run('global', function (callback) {
         callback(null, {'user':user});
     }, function (err, data) {
         console.log(arguments);
     });
+
 });
