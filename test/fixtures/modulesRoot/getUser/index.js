e426c30796ef1f1bbf55467f6d15857b99ec3ab5
Roman Bolgov <bolgovr@gmail.com>
2012-06-01 12:22:06 +0300

    add timeout

--- test/fixtures/modulesRoot/getUser/index.js
+++ test/fixtures/modulesRoot/getUser/index.js
@@ -5,16 +5,18 @@ var GetUser = function () {
   this.onStart = this.entryPoint.bind(this);
 };
 GetUser.prototype.entryPoint = function (callback, data) {
-  callback(null, {
-    'incrementViews': function (cnt) {
-      //incrementing views
-      return cnt + 1;
-    },
-    'save': function (cb) {
-      //saving user
-      cb();
-    },
-    'login': 'test'
-  });
+  setTimeout(function () {
+    callback(null, {
+      'incrementViews': function (cnt) {
+        //incrementing views
+        return cnt + 1;
+      },
+      'save': function (cb) {
+        //saving user
+        cb();
+      },
+      'login': 'test'
+    });
+  }, 20);
 };
 module.exports = GetUser;
